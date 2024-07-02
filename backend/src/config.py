3892d886eff7dae45bad7c077470fb4d9f72f93a
Shashank Daima <daimashashank10@gmail.com>
2024-07-02 22:05:52 +0530

    docker not working

--- backend/src/config.py
+++ backend/src/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 config = {
     "POSTGRES_URL": os.environ.get("POSTGRES_URL"),
     "REDIS_URL": os.environ.get("REDIS_URL"),
-    "IS_DEBUG": os.environ.get("DEBUG"),
+    "IS_DEBUG": (os.environ.get("DEBUG", "false").lower() == "true"),
 }
